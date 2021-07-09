67df3cd7a86873b5a5fc52d4ca584683e2c8609f
Tonhão Santos <ahssantos1@gmail.com>
2021-07-09 12:07:15 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Teste-novo
+# image-viewer-test
 Created with CodeSandbox
---
+++ src/components/imageViewer/BaseImageViewerCloseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="close">
+    <button @click="closeViewer">close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseImageViewerCloseButton",
+  methods: {
+    closeViewer() {
+      this.$emit("close-viewer");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.close {
+  position: absolute;
+  top: 12px;
+  right: 20px;
+  z-index: 90002;
+}
+</style>
